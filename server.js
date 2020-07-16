3b691a5730bf224be5f1082803d3c02b3d8f2f6b
torosyan1 <torosyan252@gmail.com>
2020-07-16 20:34:51 +0400

    make it better

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
 const express = require("express");
 const app = express();
-const connectDB = require("./Connections/mongoDB");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/registration", require("./API/registrationAPI"));
-app.use("/login", require("./API/loginAPI"));
-connectDB();
 
 const Port = process.env.Port || 5000;
 app.listen(Port, () => console.log("Server started ", `${Port}`));
