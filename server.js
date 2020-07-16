176882be004f4552490796ad26c52b4a69f4278f
torosyan1 <torosyan252@gmail.com>
2020-07-17 03:51:20 +0400

    vahag

--- server.js
+++ server.js
@@ -11,5 +11,14 @@ app.use("/registration", require("./API/registrationAPI"));
 app.use("/login", require("./API/loginAPI"));
 connectDB();
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server startedddddddd ", `${PORT}`));
